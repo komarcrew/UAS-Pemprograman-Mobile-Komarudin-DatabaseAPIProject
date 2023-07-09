44eeb6a9553d1e132186e1ad3fd7f0fa3e5750db
Komar Crew <bykomar97@gmail.com>
2023-07-09 15:16:18 +0700

    first commit

---
+++ app/src/main/java/com/example/komardatabaseapiproject/model/Class_Handphone.java
@@ -0,0 +1,39 @@
+package com.example.komardatabaseapiproject.model;
+/**
+ * Created by smartneasy on 01/06/17.
+ */
+public class Handphone {
+    private Integer id;
+    private String nama;
+    private String harga;
+    public Handphone() {
+        super();
+    }
+    public Handphone(Integer id, String nama, String harga
+    ) {
+        super();
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getHarga() {
+        return harga;
+    }
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+}
+public class Class_Handphone {
+}
